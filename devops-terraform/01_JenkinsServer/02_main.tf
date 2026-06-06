9421f8b37aa937649aea37498df95f90dfd0c044
mimaraslan <mimaraslan@gmail.com>
2026-06-06 15:07:35 +0300

    Terraform AWS

---
+++ devops-terraform/01_JenkinsServer/02_main.tf
@@ -0,0 +1,68 @@
+resource "aws_instance" "web" {
+  ami                    = "ami-0b6c6ebed2801a5cb"
+  instance_type          = "t3.xlarge"               
+  key_name               = "My-Ubuntu-Key"
+  vpc_security_group_ids = [aws_security_group.My-Jenkins-Server-SG.id]
+  user_data              = templatefile("./03_install.sh", {}) 
+
+  tags = {
+    Name = "My-Jenkins-Server"
+  }
+
+  root_block_device {
+    volume_size = 20
+  }
+}
+
+
+// GÖREV 1:
+// IP sabitleme Terraform ile yapılacak.
+
+
+// GÖREV 2:
+// Admin Rolü tamınlanacak.
+// EKS'yi kuracak bu makineye EC2 Admin Rolü verilecek.
+
+
+resource "aws_security_group" "My-Jenkins-Server-SG" {
+  name        = "My-Jenkins-Server-SG" #
+  description = "Allow TLS inbound traffic"
+
+  ingress = [
+    for port in [22, 80, 443, 8080, 9000, 3000] : {
+      description      = "inbound rules"
+      from_port        = port
+      to_port          = port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "My-Jenkins-Server-SG"
+  }
+
+}
+
+
+
+
+
+
+
+# EKSyi otomatikten kurmak için Create role EC2
+# Add permissions kısmından Permissions policies bu yetkiyi AdministratorAccess vereceğiz.
+# Role name : EKS_EC2_ROLE
+# Modify IAM role  - Attach an IAM role to your instance.
+
---
+++ devops-terraform/01_JenkinsServer/03_install.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+sudo apt update
+sudo apt upgrade  -y
+
+
+#java
+sudo apt install openjdk-21-jdk -y
+/usr/bin/java –version
+
+
+#jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+https://pkg.jenkins.io/debian/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+https://pkg.jenkins.io/debian binary/ | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins   -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+
+#docker
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo usermod -a -G docker $USER
+#sudo usermod -aG docker ubuntu
+sudo usermod -aG docker jenkins  
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+
+
+#sonarqube
+docker run -d --name sonar -p 9000:9000   --restart unless-stopped  sonarqube:latest
+
+# Bilgisayar acildigida Docker'in uzerinde calisan sonarqube containerlarıni otomatik çalıştırma yapilacak.
+# docker ps -a
+# docker start SONAR_CONTAINER_ID
+
+#docker run -d
+#  --name sonarqube \
+#  -p 9000:9000 \
+#  --restart unless-stopped \
+#  sonarqube:latest
+
+
+#trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
+
+
+#curl
+sudo apt install curl
+
+
+#awscliv2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
+unzip awscliv2.zip
+sudo ./aws/install
+
+
+#kubectl
+curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
+#eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+cd /tmp
+sudo mv /tmp/eksctl /bin
+
+
+# EKS nodeları burada kurulu değil.
+sudo apt update
+sudo apt upgrade -y
+
+#  EKSyi kuracak olan makineye admin rolü vermek lazım.
+#  EKSyi de Terraform üzerinden kuruyoruz. En az 1 node gerekli.
+#eksctl create cluster --name my-workspace-cluster \
+# --region us-east-1 \
+# --node-type t3.xlarge \
+# --nodes 2
+
+
+### Helm kurulumu
+#curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+#chmod 700 get_helm.sh
+#./get_helm.sh
+## helm version
+#
+#helm repo add stable https://charts.helm.sh/stable
+#helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+#
+#kubectl create namespace prometheus
+#helm install stable prometheus-community/kube-prometheus-stack -n prometheus
+#kubectl get pods -n prometheus
+
+### ArgoCD kurulumu
+#
+
+# makineyi yeniden baslatma en sonda olacak.
+ sudo reboot
